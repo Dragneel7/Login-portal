2e3b0424807fee11c3e0ac1d0c65bfe8c1cc486a
surya saini <sainisurya.1@gmail.com>
2017-07-03 22:13:07 +0530

    input field issue resolved

--- login_app/forms.py
+++ login_app/forms.py
@@ -3,12 +3,17 @@ from django import forms
 from .models import UserDetails
 
 class UserDetailsForm(forms.ModelForm):
-	user_password = forms.CharField(widget=forms.PasswordInput)
+#	user_password = forms.CharField(widget=forms.PasswordInput)
+	user_name = forms.CharField(widget=forms.TextInput(attrs = {'placeholder' : 'Username'}))
+	user_password = forms.CharField(widget=forms.PasswordInput(attrs = {'placeholder' : 'Password'}))
 	
 	class Meta:
 		model = UserDetails
 		fields = ('user_name','user_password',)
 	#	widgets = {
 	#	    'user_name':forms.TextInput(attr={'placeholder':'UserName'}),
-
+#		user_password = forms.CharField(widget=forms.PasswordInput)
 	
+#		widgets = {
+#			'user_password':forms.PasswordInput(),
+	#	}
